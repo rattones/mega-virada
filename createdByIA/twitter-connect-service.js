f3823ef43bf94107d4b3e8054b514da3b252b2b5
Marcelo <rattones@marceloratton.com>
2025-04-23 12:20:03 -0300

    update fix

--- createdByIA/twitter-connect-service.js
+++ createdByIA/twitter-connect-service.js
@@ -5,12 +5,18 @@ const Generator = require('../src/generator.js');
 const SqliteManipulation = require('../src/sqlite-manipulation.js');
 
 // Twitter client configuration
-const client = new TwitterApi({
+const clientOA1 = new TwitterApi({
+    clientId: process.env.TWITTER_CLIENT_ID,
+    clientSecret: process.env.TWITTER_CLIENT_SECRET,
+    bearerToken: process.env.TWITTER_BEARER_TOKEN,
+});
+
+const clientOA2 = new TwitterApi({
   appKey: process.env.TWITTER_API_KEY,
   appSecret: process.env.TWITTER_API_KEY_SECRET,
   accessToken: process.env.TWITTER_ACCESS_TOKEN,
   accessSecret: process.env.TWITTER_ACCESS_SECRET,
-});
+})
 
 function showBet(bet) {
   bet = bet.map(i => i.toString().padStart(2, ' '))
@@ -19,7 +25,7 @@ function showBet(bet) {
 
 async function tweetBet(betString) {
   try {
-    await client.v2.tweet(`Mega da Virada - Sugestão de aposta:\n${betString}`);
+    await clientOA2.v2.tweet(`Mega da Virada - Sugestão de aposta:\n${betString}`);
     console.log('Tweet posted successfully!');
   } catch (error) {
     console.log('Error posting tweet:', error);
@@ -46,21 +52,24 @@ async function updateDataBase() {
 }
 
 async function getDM() {
-  const eventTimeline = await client.v2.listDmEvents()
+  const eventTimeline = await clientOA1.v2.listDmEvents() // error 401 unauthorized
   console.log(eventTimeline.events)
 }
 
 async function getMentions() {
   // const tweetsOfJack = await client.v2.userMentionTimeline('34467533', { end_time: '2025-02-05T00:00:00' }); // error 429 too many requests
-  const mentionTimeline = await client.v1.mentionTimeline({ trim_user: true }); // error 453 access level
+  const mentionTimeline = await clientOA2.v1.mentionTimeline({ trim_user: true }); // error 453 access level
   const fetchedTweets = mentionTimeline.tweets;
   console.log('getMentions', fetchedTweets);
 }
 
 async function main() {
-  // await updateDataBase();
-  // await generateBets();
+  await updateDataBase();
+  await generateBets();
+  await getDM();
   await getMentions();
 }
 
-main();
+// main();
+getDM();
+// getMentions();
